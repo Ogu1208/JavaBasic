46d3a3a413031743cbe729e3dc2138872759dea3
Ogu1208 <kdasunb@naver.com>
2022-02-26 21:27:11 +0900

    Ch04 Ex

---
+++ Ch04/src/Ex4_13.java
@@ -0,0 +1,16 @@
+
+public class Ex4_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		int i = 1;
+		while(sum < 100) {
+			System.out.printf("i = %d, sum = %d%n", i-1, sum);
+			sum += i++;
+			
+		}
+		
+	}
+
+}
---
+++ Ch04/src/Ex4_6.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Ex4_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("현재 월을 입력하세요 >");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		
+		switch(month) {
+		case 3: case 4: case 5:
+			System.out.println("현재의 계절은 봄");
+			break;
+		case 6: case 7: case 8:
+			System.out.println("현재의 계절은 여름");
+			break;
+		case 9: case 10: case 11:
+			System.out.println("현재의 계절은 가을");
+			break;
+		default:
+			System.out.println("현재의 계절은 겨울");
+		}
+	}
+
+}
