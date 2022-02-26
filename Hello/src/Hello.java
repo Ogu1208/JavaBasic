a820851207c9f1e5a88b2b71ed880f0d4d837574
Ogu1208 <kdasunb@naver.com>
2022-02-26 16:40:18 +0900

    JavaBasic

---
+++ Hello/src/Hello.java
@@ -0,0 +1,15 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, !java!");
+		System.out.println("Hello, !java!");
+		System.out.println("Hello, !java!");
+		System.out.println("Hello, !java!");
+		System.out.println("Hello, !java!");
+		System.out.println("Hello, !java!");S
+		System.out.println();
+	}
+
+}
