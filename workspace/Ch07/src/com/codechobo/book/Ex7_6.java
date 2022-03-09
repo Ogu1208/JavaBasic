7bfbc90a30fbf98405345d6158ab339a72a24abd
Ogu1208 <kdasunb@naver.com>
2022-03-09 21:27:52 +0900

    Ch07 ~20T

---
+++ workspace/Ch07/src/com/codechobo/book/Ex7_6.java
@@ -0,0 +1,14 @@
+package com.codechobo.book;
+import static java.lang.Integer.*;
+import static java.lang.Math.*;
+import static java.lang.System.out;
+
+public class Ex7_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.println(random());
+		out.println("Math.Pi : " + PI);
+	}
+
+}
