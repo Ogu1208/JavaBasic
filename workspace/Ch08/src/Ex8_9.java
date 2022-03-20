df67b739cfd926a41465d8c167acc2e3a8d4c718
Ogu1208 <kdasunb@naver.com>
2022-03-20 23:52:39 +0900

    8_9

---
+++ workspace/Ch08/src/Ex8_9.java
@@ -0,0 +1,17 @@
+
+public class Ex8_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		method1();
+	}
+	
+	static void method1() throws Exception {
+		method2();
+	}
+	
+	static void method2() throws Exception {
+		throw new Exception();
+	}
+
+}
