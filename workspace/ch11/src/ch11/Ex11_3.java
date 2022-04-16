b2074d93312f53ea8e1f572a1935e1d83848c520
Ogu1208 <kdasunb@naver.com>
2022-04-16 21:01:25 +0900

    Update Ex11_3.java

--- workspace/ch11/src/ch11/Ex11_3.java
+++ workspace/ch11/src/ch11/Ex11_3.java
@@ -16,23 +16,23 @@ public class Ex11_3 {
 		
 		System.out.println("expression:" + expression);
 		
-		try {
+		try {   // Exception 설치하기 위한 try-catch
 			for(int i = 0; i < expression.length(); i++) {
 				char ch = expression.charAt(i);
 				
-				if(ch=='(') {
+				if(ch=='(') {    // '('를 만나면 push
 					st.push(ch+"");
-				}else if(ch==')') {
+				}else if(ch==')') {   // 짝꿍 ')'를 만나면 push
 					st.pop();
 				}
 			}
 			
-			if(st.isEmpty()) {
+			if(st.isEmpty()) {   // 스택이 비었으면 괄호 일치
 				System.out.println("괄호가 일치합니다.");
 			} else {
 				System.out.println("괄호가 일치하지 않습니다.");
 			} 
-		} catch(EmptyStackException e) {
+		} catch(EmptyStackException e) {   // 스택이 비었는데 pop하는경우 Exception 발생
 			System.out.println("괄호가 일치하지 않습니다.");
 		} // try
 	}
